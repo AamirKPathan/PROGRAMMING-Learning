1aa4a55045fd5bc3463c2ec8652c557867d2e3bd
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-02-16 23:55:48 +0000

    Feb 16, 2026, 6:55 PM

--- University Of British Columbia - Micro Masters Program/UBCx HtC1x University Of British Columbia - How To Code - Simple Data/1a Beginner Student Language/6.Booleans And If Expressions/Part 3/notes.rkt
+++ University Of British Columbia - Micro Masters Program/UBCx HtC1x University Of British Columbia - How To Code - Simple Data/1a Beginner Student Language/6.Booleans And If Expressions/Part 3/notes.rkt
@@ -1,3 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname notes) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
 ;if expressions
 (define WIDTH 100)
 (define HEIGHT 100)
@@ -10,15 +15,5 @@
 
 (if(< (image-width I2)
       (image-height I2))
-      (image-width I2)
-      (image-height I2))
-
-(if(< (image-width I2)
-      (image-height I2))
-      (image-width I2)
-      (image-height I2))
-    
-(if(< (image-width I1)
-      (image-height I1))
-      (image-width I2)
-      (image-height I2))
+   (image-width I2)
+   (image-height I2))
