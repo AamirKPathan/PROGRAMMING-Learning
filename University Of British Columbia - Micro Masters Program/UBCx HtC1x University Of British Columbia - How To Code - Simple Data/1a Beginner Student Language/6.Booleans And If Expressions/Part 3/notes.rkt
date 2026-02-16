4b3721fce4492cfc5b683eba19d06908f5d6f8a5
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-02-16 23:51:07 +0000

    Feb 16, 2026, 6:51 PM

--- University Of British Columbia - Micro Masters Program/UBCx HtC1x University Of British Columbia - How To Code - Simple Data/1a Beginner Student Language/6.Booleans And If Expressions/Part 3/notes.rkt
+++ University Of British Columbia - Micro Masters Program/UBCx HtC1x University Of British Columbia - How To Code - Simple Data/1a Beginner Student Language/6.Booleans And If Expressions/Part 3/notes.rkt
@@ -6,14 +6,14 @@
 (= WIDTH HEIGHT)
 
 (define I1(rectangle 10 20 "solid" "red"))
-(define I2(rectangle 20 10 "solid" "red"))
+(define I2(rectangle 20 10 "solid" "blue"))
 
 (if(< (image-width I2)
       (image-height I2))
       (image-width I2)
       (image-height I2))
 
-(if(< (image-width I1)
+(if(< (image-width I2)
       (image-height I1))
       (image-width I2)
       (image-height I2))
