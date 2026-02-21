9f068a7c2b0642a5a5e871f25f747604064dd6e4
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-02-21 23:16:51 +0000

    Feb 21, 2026, 6:16 PM

--- BroCode/WebDevelopment/REACT/REACT/src/Basics/9.onChangeEventHandler/MyComponent.jsx
+++ BroCode/WebDevelopment/REACT/REACT/src/Basics/9.onChangeEventHandler/MyComponent.jsx
@@ -5,7 +5,7 @@ import React, {useState} from 'react';
 function MyComponent() {
  
     const [name, setName] = useState("Guest");
-    const [quantity, setQuantity] = useState(1);
+    const [quantity, setQuantity] = useState("Number");
     function handleNameChange(event){
         setName(event.target.value);
     }
