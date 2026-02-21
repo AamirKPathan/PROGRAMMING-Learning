5f926faa783f721105ca29d4e32b12af3d209c4f
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-02-21 23:17:14 +0000

    Feb 21, 2026, 6:17 PM

--- BroCode/WebDevelopment/REACT/REACT/src/Basics/9.onChangeEventHandler/MyComponent.jsx
+++ BroCode/WebDevelopment/REACT/REACT/src/Basics/9.onChangeEventHandler/MyComponent.jsx
@@ -9,11 +9,15 @@ function MyComponent() {
     function handleNameChange(event){
         setName(event.target.value);
     }
-    
+    function handleQuantityChange(event){
+        setQuantity(event.target.value);
+    }
 
     return(<div>
         <input value = {name} onChange = {handleNameChange}/>
         <p>Name: {name}</p>
+        <input value = {quantity} onChange = {handleQuantityChange}/>
+        <p>Quantity: {quantity}</p>
     </div>);
 }
 export default MyComponent
