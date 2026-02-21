c5f69b0fad816fa82c809ecfd31158dc9bac38bc
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-02-21 21:17:16 +0000

    Feb 21, 2026, 4:17 PM

--- BroCode/WebDevelopment/REACT/REACT/src/Basics/9.onChangeEventHandler/MyComponent.jsx
+++ BroCode/WebDevelopment/REACT/REACT/src/Basics/9.onChangeEventHandler/MyComponent.jsx
@@ -6,6 +6,11 @@ function MyComponent() {
  
     const [name, setName] = useState('');
 
-    return(<></>)
+
+
+    return(<div>
+        <input type="text" placeholder="Enter your name" onChange={(e) => setName(e.target.value)}/>
+        <p>Hello {name}</p>
+    </div>)
 }
 export default MyComponent
