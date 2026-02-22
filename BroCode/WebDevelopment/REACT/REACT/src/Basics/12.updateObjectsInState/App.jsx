0da8f1e0a8535c1f76f2062d1e15d10fbf233fb1
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-02-22 23:31:21 +0000

    Feb 22, 2026, 6:31 PM

---
+++ BroCode/WebDevelopment/REACT/REACT/src/Basics/12.updateObjectsInState/App.jsx
@@ -0,0 +1,12 @@
+import MyComponent from "./MyComponent";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="app">
+      <MyComponent />
+    </div>
+  );
+}
+
+export default App;
