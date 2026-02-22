7aca25f741e8c99ad7053e4687cc52559126abd7
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-02-22 16:34:37 +0000

    Feb 22, 2026, 11:34 AM

--- BroCode/WebDevelopment/REACT/REACT/src/lessonSelector.js
+++ BroCode/WebDevelopment/REACT/REACT/src/lessonSelector.js
@@ -1,5 +1,5 @@
 export const currentLesson = 0;
-export const currentProject = "Color Picker App";
+export const currentProject = "colorPickerApp";
 
 export const lessonFolders = {
   1: "1.reactTutorialForBeginners",
