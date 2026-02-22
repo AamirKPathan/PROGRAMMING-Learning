9ba181e2e098e16ac83ff17490991d74e1b41b0a
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-02-22 23:32:32 +0000

    Feb 22, 2026, 6:32 PM

--- BroCode/WebDevelopment/REACT/REACT/src/Basics/12.updateObjectsInState/MyComponent.jsx
+++ BroCode/WebDevelopment/REACT/REACT/src/Basics/12.updateObjectsInState/MyComponent.jsx
@@ -1,1 +1,11 @@
-import React,
+import React, { useState } from 'react';
+
+function MyComponent() {
+    const [car, setCar] = useState({
+        year: 2025,
+        make: "Lamborghini",
+        model: "Huracan"
+    });
+
+    
+}
