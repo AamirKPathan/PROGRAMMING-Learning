bea43cad97f8e5a3ded1ddaf7989a08f38f1641f
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-02-22 16:44:07 +0000

    Feb 22, 2026, 11:44 AM

--- BroCode/WebDevelopment/REACT/REACT/src/Projects/colorPickerApp/App.jsx
+++ BroCode/WebDevelopment/REACT/REACT/src/Projects/colorPickerApp/App.jsx
@@ -6,13 +6,11 @@ function App() {
 
   return (
     <div className="app">
-      <div className="square">
+      <div className="square" style={{ backgroundColor: color }}>
         <h1 className="main-heading">Color Picker</h1>
 
-        <div className="selected">
-          <p className="label">Selected Color:</p>
-          <p className="hex">{color}</p>
-        </div>
+        <p className="label">Selected Color:</p>
+        <p className="hex">{color}</p>
 
         <MyComponent color={color} setColor={setColor} />
       </div>
