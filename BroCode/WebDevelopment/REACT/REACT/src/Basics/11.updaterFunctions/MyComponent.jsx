27c399ebd3b7022df2196d3a3083469d4da8d9ad
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-02-22 17:27:53 +0000

    Feb 22, 2026, 12:27 PM

--- BroCode/WebDevelopment/REACT/REACT/src/Basics/11.updaterFunctions/MyComponent.jsx
+++ BroCode/WebDevelopment/REACT/REACT/src/Basics/11.updaterFunctions/MyComponent.jsx
@@ -1,2 +1,26 @@
 // updater function = A function that updates the state based on the previous state
 
+function MyComponent() {
+    const [count, setCount] = useState(0);
+
+    function increment() {
+        setCount(prevCount => prevCount + 1);
+    };
+
+    function decrement() {
+        setCount(prevCount => prevCount - 1);
+    };
+
+    function reset() {
+        setCount(0);
+    };
+
+    return (
+        <div className="counter">
+            <h1>Counter: {count}</h1>
+            <button onClick={increment}>+</button>
+            <button onClick={decrement}>-</button>
+            <button onClick={reset}>Reset</button>
+        </div>
+    );
+}
