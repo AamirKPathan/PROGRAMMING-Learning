515c3f3f69edd4d9b6fc26a7303cd2d423083a86
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-03-10 00:44:52 +0000

    Mar 9, 2026, 8:44 PM

---
+++ BroCode/WebDevelopment/REACT/REACT/src/Basics/13.updateArraysInState/MyComponent.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function MyComponent() {
+    const [foods, setFoods] = useState(['Pizza', 'Burger', 'Pasta']);
+
+    functi
+
+    return(<></>);
+}
+
+export default MyComponent;
