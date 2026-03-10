39a0dc7eccb15c11d7cfb2623c7c42aeca1bbd3e
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-03-10 00:50:19 +0000

    Mar 9, 2026, 8:50 PM

--- BroCode/WebDevelopment/REACT/REACT/src/Basics/13.updateArraysInState/MyComponent.jsx
+++ BroCode/WebDevelopment/REACT/REACT/src/Basics/13.updateArraysInState/MyComponent.jsx
@@ -8,7 +8,7 @@ function MyComponent() {
         const newFood = document.getElementById('foodInput').value;
         document.getElementById('foodInput').value = "";
 
-        setFoods(f => [...foods, newFood]);
+        setFoods(f => [...f, newFood]);
     }
 
     function handleRemoveFood() {
