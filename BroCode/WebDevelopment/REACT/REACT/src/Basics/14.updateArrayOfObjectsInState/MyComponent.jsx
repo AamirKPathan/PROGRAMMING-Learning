bd4663ebda6afaa67ec4359a3646a3e8fc8638c3
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-03-21 16:55:32 +0000

    Mar 21, 2026, 12:55 PM

--- BroCode/WebDevelopment/REACT/REACT/src/Basics/14.updateArrayOfObjectsInState/MyComponent.jsx
+++ BroCode/WebDevelopment/REACT/REACT/src/Basics/14.updateArrayOfObjectsInState/MyComponent.jsx
@@ -32,7 +32,9 @@ function MyComponent() {
         <ul>
             {cars.map((car, index) => (
                 <li key={index}>
-                    {`${car.year} ${car.make} ${car.model}`}
+                    {`${car.year} 
+                    ${car.make} 
+                    ${car.model}`}
                 </li>
             ))}
         </ul>
