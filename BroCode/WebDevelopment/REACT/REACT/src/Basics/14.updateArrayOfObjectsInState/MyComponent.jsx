98dd70defcbce4418d66983bcefa660a384be966
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-03-21 16:44:38 +0000

    Mar 21, 2026, 12:44 PM

--- BroCode/WebDevelopment/REACT/REACT/src/Basics/14.updateArrayOfObjectsInState/MyComponent.jsx
+++ BroCode/WebDevelopment/REACT/REACT/src/Basics/14.updateArrayOfObjectsInState/MyComponent.jsx
@@ -2,7 +2,7 @@ import React, { use, useState } from 'react';
 
 function MyComponent() {
     const [cars, setCars] = useState([])
-    const [carYear, setCarYear] = useState();
+    const [carYear, setCarYear] = useState(new Date().getFullYear());
 }
 
 export default MyComponent;
