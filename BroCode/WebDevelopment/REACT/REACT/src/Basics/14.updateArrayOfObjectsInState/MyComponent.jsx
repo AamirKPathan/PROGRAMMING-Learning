b775be9e3ec1d620553be938deed1b0a9838bf5c
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-03-21 16:51:43 +0000

    Mar 21, 2026, 12:51 PM

--- BroCode/WebDevelopment/REACT/REACT/src/Basics/14.updateArrayOfObjectsInState/MyComponent.jsx
+++ BroCode/WebDevelopment/REACT/REACT/src/Basics/14.updateArrayOfObjectsInState/MyComponent.jsx
@@ -31,7 +31,9 @@ function MyComponent() {
         <h2>List Of Car Objects</h2>
         <ul>
             {cars.map((car, index) => (
-                <li key={index}>{car.year} {car.make} {car.model} <button onClick={() => HandleRemoveCar(index)}>Remove</button></li>
+                <li key={index}>
+                    {car.year} 
+                    {car.make} {car.model} <button onClick={() => HandleRemoveCar(index)}>Remove</button></li>
             ))}
         </ul>
         <input type="number" placeholder='Year' value={carYear} onChange={HandleYearChange}/>
