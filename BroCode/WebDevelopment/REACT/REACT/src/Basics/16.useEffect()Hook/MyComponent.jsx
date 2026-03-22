24a17cc81d8f943dde1b2570f65601b22eef5a7e
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-03-22 17:54:36 +0000

    Mar 22, 2026, 1:54 PM

--- BroCode/WebDevelopment/REACT/REACT/src/Basics/16.useEffect()Hook/MyComponent.jsx
+++ BroCode/WebDevelopment/REACT/REACT/src/Basics/16.useEffect()Hook/MyComponent.jsx
@@ -19,9 +19,11 @@ function MyComponent() {
     
     const [count, setCount] = useState(0);
 
+
+    
     return(<>
         <h1>Count: {count}</h1>
-        
+        <button onClick={addCount}>Add</button>
     </>)
 }
 
