ba4a8e445c877b94250d8d3ad985eb0005cb3d7d
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-03-22 17:05:36 +0000

    Mar 22, 2026, 1:05 PM

--- BroCode/WebDevelopment/REACT/REACT/src/Basics/16.useEffect()Hook/MyComponent.jsx
+++ BroCode/WebDevelopment/REACT/REACT/src/Basics/16.useEffect()Hook/MyComponent.jsx
@@ -1,55 +1,7 @@
 import React, { use, useState } from 'react';
 
 function MyComponent() {
-    const [cars, setCars] = useState([])
-    const [carYear, setCarYear] = useState(new Date().getFullYear());
-    const [carMake, setCarMake] = useState("");
-    const [carModel, setCarModel] = useState("");
-
-    // Declare Functions
-    function HandleAddCar() {
-        const newCar = {
-            year: carYear,
-            make: carMake,
-            model: carModel
-        };
-        setCars(c => [...c, newCar]);
-        
-        setCarYear(new Date().getFullYear());
-        setCarMake("");
-        setCarModel("");
-    }
-    function HandleRemoveCar(index) {
-        setCars(c => c.filter((_, i) => i !== index));
-    }
-    function HandleYearChange(event) {
-        setCarYear(event.target.value);
-    }
-    function HandleMakeChange(event) {
-        setCarMake(event.target.value);
-    }
-    function HandleModelChange(event) {
-        setCarModel(event.target.value);
-    }
-    return(<div>
-        <h2>List Of Car Objects</h2>
-        <ul>
-            {cars.map((car, index) => (
-                <li key={index} onClick={() => HandleRemoveCar(index)}>
-                    {`${car.year} 
-                    ${car.make} 
-                    ${car.model}`}
-                </li>
-            ))}
-        </ul>
-        <input type="number" placeholder='Year' value={carYear} onChange={HandleYearChange}/>
-        <br/>
-        <input type="text" placeholder='Make' value={carMake} onChange={HandleMakeChange}/>
-        <br/>
-        <input type="text" placeholder='Model' value={carModel} onChange={HandleModelChange}/>
-        <br/>
-        <button onClick={HandleAddCar}>Add Car</button>
-    </div>)
+    
 }
 
 export default MyComponent;
