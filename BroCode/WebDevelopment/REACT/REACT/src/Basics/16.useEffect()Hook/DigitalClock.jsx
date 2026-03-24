e46817890e1e1d08d02e0b0cc2299af6cf2be71e
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-03-24 00:37:39 +0000

    Mar 23, 2026, 8:37 PM

--- BroCode/WebDevelopment/REACT/REACT/src/Basics/16.useEffect()Hook/DigitalClock.jsx
+++ BroCode/WebDevelopment/REACT/REACT/src/Basics/16.useEffect()Hook/DigitalClock.jsx
@@ -3,9 +3,9 @@ import React, { useState, useEffect } from 'react';
 function DigitalClock(){
 
     return(
-    <div className= "clock-container">
-        <div>
-
+    <div className = "clock-container">
+        <div className = "clock">
+            <span>00:00:</span>
         </div>
     </div>);
 
