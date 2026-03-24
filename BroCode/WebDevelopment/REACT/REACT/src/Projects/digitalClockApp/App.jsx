276c7694e15c5927b378ee96b3c8ecf95b4302c2
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-03-24 00:54:20 +0000

    Mar 23, 2026, 8:54 PM

---
+++ BroCode/WebDevelopment/REACT/REACT/src/Projects/digitalClockApp/App.jsx
@@ -0,0 +1,12 @@
+import DigitalClock from "./DigitalClock";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="app">
+      <DigitalClock />
+    </div>
+  );
+}
+
+export default App;
