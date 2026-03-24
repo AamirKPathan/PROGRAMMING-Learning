0581d182fde3bbc0cd6cdd9080832e97606e3f39
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-03-24 00:51:27 +0000

    Mar 23, 2026, 8:51 PM

--- BroCode/WebDevelopment/REACT/REACT/src/Basics/17.digitalClockApp/DigitalClock.jsx
+++ BroCode/WebDevelopment/REACT/REACT/src/Basics/17.digitalClockApp/DigitalClock.jsx
@@ -14,10 +14,10 @@ function DigitalClock() {
     []);
 
     function formatTime(){
-        const hours = time.getHours().toString().padStart(2, "0");
-        const minutes = time.getMinutes().toString().padStart(2, "0");
-        const seconds = time.getSeconds().toString().padStart(2, "0");
-        return `${hours}:${minutes}:${seconds}`;
+        let hours = time.getHours();
+        let minutes = time.getMinutes();
+        let seconds = time.getSeconds();
+        
     }
 
     return (
