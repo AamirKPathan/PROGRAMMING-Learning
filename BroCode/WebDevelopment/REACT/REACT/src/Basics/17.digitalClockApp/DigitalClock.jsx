4bc2640c863c8157a11502a2df5d1a12df585df0
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-03-24 00:41:32 +0000

    Mar 23, 2026, 8:41 PM

--- BroCode/WebDevelopment/REACT/REACT/src/Basics/17.digitalClockApp/DigitalClock.jsx
+++ BroCode/WebDevelopment/REACT/REACT/src/Basics/17.digitalClockApp/DigitalClock.jsx
@@ -1,7 +1,23 @@
 import React, { use, useState } from 'react';
 
 function DigitalClock() {
+    const [time, setTime] = useState(new Date());
 
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => clearInterval(timer);
+    }, []);
+
+    return (
+        <div className="clock-container">
+            <div className="clock">
+                <span>{time.toLocaleTimeString()}</span>
+            </div>
+        </div>
+    );
 }
 
-export default ToDoList;
+export default DigitalClock;
