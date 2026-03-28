b95a3b95614117f34245c4e36d409c119e9ed40e
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-03-28 20:26:41 +0000

    Mar 28, 2026, 4:26 PM

--- BroCode/WebDevelopment/REACT/REACT/src/Basics/19.useRef()Hook/MyComponent.jsx
+++ BroCode/WebDevelopment/REACT/REACT/src/Basics/19.useRef()Hook/MyComponent.jsx
@@ -2,7 +2,9 @@ import React, {useState, useContext, useRef} from "react";
 function ComponentD(){
 
     let [number, setNumber] = useState(0);
-    function Ha
+    function handleClick(){
+        setNumber(n => n + 1)
+    }
     return(
         <button onClick={handleClick}>
             Click Me!
