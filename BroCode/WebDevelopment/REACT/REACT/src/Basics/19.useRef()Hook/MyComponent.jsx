628b14e15aa91358771a3a1aae1f23c0b71d28a3
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-03-28 20:26:15 +0000

    Mar 28, 2026, 4:26 PM

--- BroCode/WebDevelopment/REACT/REACT/src/Basics/19.useRef()Hook/MyComponent.jsx
+++ BroCode/WebDevelopment/REACT/REACT/src/Basics/19.useRef()Hook/MyComponent.jsx
@@ -2,8 +2,15 @@ import React, {useState, useContext, useRef} from "react";
 function ComponentD(){
 
     let [number, setNumber] = useState(0);
+    const ref = useRef(null);
+
+    const handleClick = () => {
+        setNumber(number + 1);
+        ref.current.focus();
+    };
+
     return(
-        <button>
+        <button onClick={handleClick}>
             Click Me!
         </button>
     )
