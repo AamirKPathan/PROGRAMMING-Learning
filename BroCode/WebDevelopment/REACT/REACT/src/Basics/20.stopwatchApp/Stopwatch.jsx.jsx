0ccfe8d157402cede8a5805809c2131d4b8963a5
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-03-28 20:35:38 +0000

    Mar 28, 2026, 4:35 PM

--- BroCode/WebDevelopment/REACT/REACT/src/Basics/20.stopwatchApp/Stopwatch.jsx.jsx
+++ BroCode/WebDevelopment/REACT/REACT/src/Basics/20.stopwatchApp/Stopwatch.jsx.jsx
@@ -30,11 +30,11 @@ function Stopwatch(){
     };
 
     const formatTime = () => {
-        const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
         const minutes = Math.floor((time / (1000 * 60)) % 60);
         const seconds = Math.floor((time / 1000) % 60);
-        
-        return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+        const centiseconds = Math.floor((time % 1000) / 10);
+
+        return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}:${String(centiseconds).padStart(2, '0')}`;
     };
 
     return(
