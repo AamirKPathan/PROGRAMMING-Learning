42707ea3a55709a148e0717c6c8220e3930121d9
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-05-08 01:47:07 +0000

    May 7, 2026, 9:47 PM

--- BroCode/AIandMLPreReqs/Python/2. Variables/main.py
+++ BroCode/AIandMLPreReqs/Python/2. Variables/main.py
@@ -2,8 +2,8 @@
 
 # 4 Types of variables:
 # 1. String - a sequence of characters, enclosed in quotes
-name = "Aamir "
-
+firstName = "Aamir Khan"
+lastName
 # 2. Integer - a whole number, positive or negative, without decimals
 # 3. Float - a number, positive or negative, with decimals
 # 4. Boolean - a value that is either True or False
