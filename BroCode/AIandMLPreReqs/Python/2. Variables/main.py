b00afe22f5c48ab4d1cc43bafbc10759c5a75660
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-05-08 01:46:56 +0000

    May 7, 2026, 9:46 PM

--- BroCode/AIandMLPreReqs/Python/2. Variables/main.py
+++ BroCode/AIandMLPreReqs/Python/2. Variables/main.py
@@ -1,3 +1,9 @@
 # Variable = a container for a value. It can be changed after it has been created.
 
-# 4 Types
+# 4 Types of variables:
+# 1. String - a sequence of characters, enclosed in quotes
+name = "Aamir "
+
+# 2. Integer - a whole number, positive or negative, without decimals
+# 3. Float - a number, positive or negative, with decimals
+# 4. Boolean - a value that is either True or False
