d62e7ef4b5ee005e036e27ec3502e53e6b3b3146
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-05-08 01:48:19 +0000

    May 7, 2026, 9:48 PM

--- BroCode/AIandMLPreReqs/Python/2. Variables/main.py
+++ BroCode/AIandMLPreReqs/Python/2. Variables/main.py
@@ -2,10 +2,7 @@
 
 # 4 Types of variables:
 # 1. String - a sequence of characters, enclosed in quotes
-firstName = "Aamir Khan"
-lastName = "Pathan"
-fullName = firstName + " " + lastName
-print(
+fullName
 # 2. Integer - a whole number, positive or negative, without decimals
 age = 16
 # 3. Float - a number, positive or negative, with decimals
