3c875cf91408a50c0ddeb42fbeef302b600f4f3f
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-05-09 21:10:13 +0000

    May 9, 2026, 5:10 PM

--- BroCode/AIandMLPreReqs/Python/Learning/5. Madlibs/madlibs.py
+++ BroCode/AIandMLPreReqs/Python/Learning/5. Madlibs/madlibs.py
@@ -1,5 +1,5 @@
 # Madlibs
 
 # Template
-
+print(f"Today I went to the {input('Place: ')} and bought a {input('Adjective: ')} {input('Noun: ')}. It was very {input('Adjective: ')} and I had a great time!")
 # User Input
