ba9f4dc5c2ec5c422f5c2a519a58189ccf94a8c1
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-05-09 21:12:34 +0000

    May 9, 2026, 5:12 PM

---
+++ BroCode/AIandMLPreReqs/Python/Projects/5. Madlibs/madlibs.py
@@ -0,0 +1,11 @@
+# Madlibs
+# Welcome Message
+print("Welcome to the Madlibs Game!🎲🔠")
+
+# User Input
+place1 = input("Enter a place: ")
+adjective1 = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+adjective2 = input("Enter another adjective: ")
+# Template
+print(f"Today I went to the {place1} and bought a {adjective1} {noun1}. It was very {adjective2} and I had a great time!")
