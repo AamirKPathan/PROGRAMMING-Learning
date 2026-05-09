24425bf5dcd22588a95a08c5288c593ed1f56f9c
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-05-09 20:35:47 +0000

    May 9, 2026, 4:35 PM

---
+++ BroCode/AIandMLPreReqs/Python/Learning/4. User Input/main.py
@@ -0,0 +1,7 @@
+# input()
+# prompts the user for input and returns it as a string
+name = input("What is your name? ")
+age = input("What is your age? ")
+gpa = input("What is your GPA? ")
+isStudent = input("Are you a student? (True/False) ")
+
