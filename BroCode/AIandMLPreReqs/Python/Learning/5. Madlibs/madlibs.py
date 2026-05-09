33607574242cef645555bc38b388b054b4d02da3
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-05-09 21:11:21 +0000

    May 9, 2026, 5:11 PM

--- BroCode/AIandMLPreReqs/Python/Learning/5. Madlibs/madlibs.py
+++ BroCode/AIandMLPreReqs/Python/Learning/5. Madlibs/madlibs.py
@@ -3,3 +3,7 @@
 # Template
 print(f"Today I went to the {place1} and bought a {adjective1} {noun1}. It was very {adjective2} and I had a great time!")
 # User Input
+place1 = input("Enter a place: ")
+adjective1 = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+adjective2 = input("Enter another adjective: ")
