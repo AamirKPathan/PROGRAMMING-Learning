85d8bf231ba4769c0ce7c3d3bd4eb01001bbf5dc
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-05-09 20:35:58 +0000

    May 9, 2026, 4:35 PM

--- BroCode/AIandMLPreReqs/Python/Learning/4. User Input/main.py
+++ BroCode/AIandMLPreReqs/Python/Learning/4. User Input/main.py
@@ -5,3 +5,7 @@ age = input("What is your age? ")
 gpa = input("What is your GPA? ")
 isStudent = input("Are you a student? (True/False) ")
 
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"GPA: {gpa}")
+print(f"Is Student: {isStudent}")
